1fd974e6d5ebdc0e17639e828827d42f28c62348
Joe Elias <jelias4@binghamton.edu>
2025-03-09 07:46:38 -0400

    v0.2

--- code/OrthoID.R
+++ code/OrthoID.R
@@ -1,51 +1,57 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
+library(dplyr)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+metadata<-read.csv('audio.csv', stringsAsFactors = FALSE)
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+random_row<-metadata[sample(row(metadata), 1), ]
+random_file<-random_row$filename
+correct_species<-random_row$species
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+ui<-fluidPage(
+  titlePanel('Ortho ID'), 
+  
+  sidebarLayout(
+    sidebarPanel(
+      actionButton('new_test', 'Orthopterate!'), 
+      textInput('guess', 'enter species name'), 
+      actionButton('submit', 'submit answer'), 
+      textOutput('feedback')
+    ), 
+    mainPanel(
+      uiOutput('audio_player')
     )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+server<-function(input, output, session){
+  
+  addResourcePath("audio", file.path(getwd(), "audio"))  # Absolute path
+  quiz_data<-reactiveValues(file=NULL, species=NULL)
+  
+  # select a random file 
+  observeEvent(input$new_test, {
+    selected<-metadata[sample(nrow(metadata), 1), ]
+    quiz_data$file<-selected$filename
+    quiz_data$species<-selected$species
+  })
+  
+  # display audio player
+  output$audio_player<-renderUI({
+    req(quiz_data$file)
+    tags$audio(src=paste0('audio/', quiz_data$file), type='audio/mp3', controls=NA)
+  })
+  
+  # check answer
+  observeEvent(input$submit, {
+    req(input$guess, quiz_data$species)
+    if (tolower(input$guess)==tolower(quiz_data$species)){
+      output$feedback<-renderText("correct!")
+    } else {
+      output$feedback<-renderText(paste('wrong! correct answer = :' , quiz_data$species))
+    }
+  })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
+
