243d63ef21bf70798edaf02940cf44515d6dc208
joe-elias <joseph.m.elias1@gmail.com>
2025-03-11 16:40:25 -0400

    v0.4

--- code/OrthoID.R
+++ code/OrthoID.R
@@ -1,32 +1,22 @@
 library(shiny)
 library(dplyr)
 
-audio_folder<-'audio'
-
-audio_files<-list.files(audio_folder, pattern='\\.mp3$', full.names = FALSE)
-
-metadata<-data.frame(
-  filename=audio_files, 
-  species=gsub('_\\d+\\.mp3$', '', audio_files)
-)
-
-write.csv(metadata, 'audio_metadata.csv', row.names = FALSE)
-cat("CSV metadata file saved as audio_metadata.csv\n")
-
 metadata<-read.csv('audio.csv', stringsAsFactors = FALSE)
+
 
 random_row<-metadata[sample(row(metadata), 1), ]
 random_file<-random_row$filename
 correct_species<-random_row$species
+common_name<-random_row$common
 
-ui<-fluidPage(
+ui <- fluidPage(
   titlePanel('Ortho ID'), 
   
   sidebarLayout(
     sidebarPanel(
       actionButton('new_test', 'Orthopterate!'), 
-      textInput('guess', 'enter species name'), 
-      actionButton('submit', 'submit answer'), 
+      textInput('answer', "Answer:", value = "Gryllus rubens"),
+      actionButton('submit', 'Submit Answer'), 
       textOutput('feedback')
     ), 
     mainPanel(
@@ -36,28 +26,31 @@ ui<-fluidPage(
 )
 
 server<-function(input, output, session){
-  quiz_data<-reactiveValues(file=NULL, species=NULL)
+  quiz_data<-reactiveValues(file=NULL, species=NULL, common=NULL)
   
   # select a random file 
   observeEvent(input$new_test, {
     selected<-metadata[sample(nrow(metadata), 1), ]
     quiz_data$file<-selected$filename
     quiz_data$species<-selected$species
+    quiz_data$common<-selected$common
   })
   
   # display audio player
   output$audio_player<-renderUI({
     req(quiz_data$file)
-    tags$audio(src=paste0('audio/', quiz_data$file), type='audio/mp3', controls=NA)
+    tags$audio(src=paste0('https://raw.githubusercontent.com/joe-elias/OrthoID/main/audio/', 
+                          quiz_data$file), type='audio/mp3', controls=NA)
   })
   
   # check answer
   observeEvent(input$submit, {
-    req(input$guess, quiz_data$species)
-    if (tolower(input$guess)==tolower(quiz_data$species)){
-      output$feedback<-renderText("correct!")
+    req(input$answer, quiz_data$species)
+    if (tolower(input$answer)==tolower(quiz_data$species)){
+      output$feedback<-renderText(paste("correct!", quiz_data$species, ';', quiz_data$common))
     } else {
-      output$feedback<-renderText(paste('wrong! correct answer = :' , quiz_data$species))
+      output$feedback<-renderText(paste('wrong! correct answer = :' , 
+                                        quiz_data$species, ';', quiz_data$common))
     }
   })
 }
@@ -65,4 +58,4 @@ server<-function(input, output, session){
 shinyApp(ui = ui, server = server)
 
 # end 
-
+# next steps after we get the scientific and common to show - images of the species 
